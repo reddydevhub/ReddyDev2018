95305158c4b8c23c9ff61a2805c0bd92cb5b3f5e
reddydevhub <41873349+reddydevhub@users.noreply.github.com>
2018-07-30 12:59:58 -0400

    Adding comments

--- Test.js
+++ Test.js
@@ -1,10 +1,14 @@
+// Variable to print alert statement
 var a = alert('Test');
 
+// Function to return some value
 function v(a) {
   console.log('Input Value is: ' + a);
   return a*5;
 }
 
+// Variable to call above function
 var t = v(5);
 
+// Alert statement to print above variable value
 alert(t);
